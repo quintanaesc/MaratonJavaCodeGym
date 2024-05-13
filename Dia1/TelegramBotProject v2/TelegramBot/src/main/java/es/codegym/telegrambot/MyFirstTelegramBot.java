8e3ef25613c271a1eb6beb2fe0ed21b46b910e84
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2024-05-12 23:51:53 -0600

    trabajo del dia 1: https://www.youtube.com/watch?v=JNblWdUPbNs

---
+++ Dia1/TelegramBotProject v2/TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,31 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "yortzedBot";
+    public static final String TOKEN = "7199677960:AAEp5e66-8oveJPMDzmh-LQ6sRIsul-Il98";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+        sendTextMessageAsync("yortzed a iniciado el maraton");
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
