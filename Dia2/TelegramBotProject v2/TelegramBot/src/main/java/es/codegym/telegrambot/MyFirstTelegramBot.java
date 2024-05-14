7cbb77f113b4611ebfdf6aa8b795f609d16e543a
quintanaesc <2im3.quintana.esc.azul@gmail.com>
2024-05-13 21:37:53 -0600

    Día 2 https://www.youtube.com/watch?v=6uhWBlb9mhg

---
+++ Dia2/TelegramBotProject v2/TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,37 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "yortzedBot";
+    public static final String TOKEN = "7199677960:AAEp5e66-8oveJPMDzmh-LQ6sRIsul-Il98";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+        if(getMessageText().equals("/start")){
+            sendTextMessageAsync("yortzed a iniciado el maraton");
+        } else if (getMessageText().contains("me llamo")) {
+            sendTextMessageAsync("Encantado de conocerte, soy *Gato*");
+        }else{
+            sendTextMessageAsync("Aquí hay frase");
+        }
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Trabajos de los 5 días de duración del Maratón de CodeGym para TI en Java: https://www.youtube.com/@codegym1714/featured
 
         -Día 1 creacion de un bot en Telegram
+        -Día 2 opciones de conversacion y resaltado de texto
+        
